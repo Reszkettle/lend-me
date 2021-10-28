826f5c522fae596b448455bd17f46365d16b43bc
klima7 <ukasz.klimkiewicz2@wp.pl>
2021-10-28 17:31:52 +0200

    Frontend prepared to use firebase emulators

--- frontend/lendme/lib/main.dart
+++ frontend/lendme/lib/main.dart
@@ -1,15 +1,22 @@
-
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:lendme/screens/proxy.dart';
 import 'package:lendme/services/auth.dart';
+import 'package:lendme/services/emulators.dart';
 import 'package:provider/provider.dart';
 
 import 'models/user.dart';
 
+const useEmulators = false;
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+
+  if (useEmulators) {
+    await EmulatorsService().setupEmulators();
+  }
+
   runApp(const MyApp());
 }
 
