1066c4fdc95d73879dcf439bef5075693c0b297b
klima7 <ukasz.klimkiewicz2@wp.pl>
2021-10-28 17:27:42 +0200

    Creating user documents with backend function

--- backend/functions/src/index.ts
+++ backend/functions/src/index.ts
@@ -1,6 +1,34 @@
 import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
 
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+admin.initializeApp();
+const firestore = admin.firestore();
+
+exports.onUserCreated = functions.auth.user().onCreate(async (user) => {
+  let firstName = null;
+  let lastName = null;
+
+  if (user.displayName != null) {
+    const fullName = user.displayName;
+    const parts = fullName.split(" ");
+    firstName = parts.slice(0, -1).join(" ");
+    if (parts.length > 1) {
+      lastName = parts.slice(-1).join(" ");
+    }
+  }
+
+  const userDocument = {
+    avatarUrl: user.photoURL,
+    createdAt: admin.firestore.FieldValue.serverTimestamp(),
+    email: user.email,
+    firstName: firstName,
+    lastName: lastName,
+    phone: user.phoneNumber,
+  };
+  await firestore.collection("users").doc(user.uid).set(userDocument);
+});
+
+// It's really not so simple, temporary solution
+exports.onUserDeleted = functions.auth.user().onDelete(async (user) => {
+  await firestore.collection("users").doc(user.uid).delete();
 });
