ccbc0f99ccaed0a6264ec398ae6b4dd3a0bb49d6
klima7 <ukasz.klimkiewicz2@wp.pl>
2021-11-02 16:26:44 +0100

    Mapping exceptions tidied

--- frontend/lendme/lib/exceptions/map.dart
+++ frontend/lendme/lib/exceptions/map.dart
@@ -1,21 +1,31 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:lendme/exceptions/exception.dart';
 import 'package:lendme/exceptions/general.dart';
 
 DomainException mapToDomainException(Object e) {
   if(e is FirebaseException) {
-    if(e.code == 'network-request-failed'){
-      return InternetException();
-    }
-
-    if(e.message != null) {
-      return DomainException(e.message!);
-    }
-    else {
-      return UnknownException(cause: e);
-    }
+    return _mapFirebaseException(e);
   }
   else {
-    return UnknownException(cause: e);
+    return _mapNonFirebaseException(e);
   }
+}
+
+DomainException _mapFirebaseException(FirebaseException e) {
+  print('Mapping firebase exception: $e (code: ${e.code})');
+
+  switch(e.code) {
+    case 'network-request-failed':
+    case 'unavailable':
+      return InternetException();
+  }
+
+  // Fallback
+  return UnknownException(cause: e);
+}
+
+DomainException _mapNonFirebaseException(Object e) {
+  print('Mapping non firebase exception: $e');
+  return UnknownException(cause: e);
 }
