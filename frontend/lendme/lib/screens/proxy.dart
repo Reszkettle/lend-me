e2e531def2a90a61fc678f7d28da6a29e37a41eb
klima7 <ukasz.klimkiewicz2@wp.pl>
2021-11-03 21:04:11 +0100

    Animations added to proxy

--- frontend/lendme/lib/screens/proxy.dart
+++ frontend/lendme/lib/screens/proxy.dart
@@ -10,13 +10,40 @@ import 'package:provider/provider.dart';
 import 'auth/auth.dart';
 import 'main/main.dart';
 
-class Proxy extends StatelessWidget {
+class Proxy extends StatefulWidget {
   const Proxy({Key? key}) : super(key: key);
+
+  @override
+  State<Proxy> createState() => _ProxyState();
+}
+
+class _ProxyState extends State<Proxy> {
+
+  late Widget _view;
+
+  @override
+  void initState() {
+    _view = _getProperScreen(Resource<User?>.loading());
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     final userResource = Provider.of<Resource<User?>>(context);
-    return _getProperScreen(userResource);
+    setState(() {
+      _view = _getProperScreen(userResource);
+    });
+    return AnimatedSwitcher(
+      duration: const Duration(milliseconds: 700),
+      transitionBuilder: (Widget child, Animation<double> animation) {
+        return SlideTransition(
+          position: Tween<Offset>(begin: const Offset(1.2, 0), end: const Offset(0, 0))
+              .animate(animation),
+          child: child,
+        );
+      },
+      child: _view,
+    );
   }
 
   Widget _getProperScreen(Resource<User?> userResource) {
