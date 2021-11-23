bc0c093ebb0434a1e7b207035840e4994a2de1f2
klima7 <ukasz.klimkiewicz2@wp.pl>
2021-11-23 21:06:16 +0100

    Details screen for available items done

--- frontend/lendme/lib/screens/other/item_details/panel_available.dart
+++ frontend/lendme/lib/screens/other/item_details/panel_available.dart
@@ -8,14 +8,53 @@ class PanelAvailable extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return _mainLayout();
+  }
+
+  Column _mainLayout() {
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        _header(),
+        const SizedBox(height: 16),
+        _buttons(),
+      ],
+    );
+  }
+
+  Widget _header() {
+    return Row(
       children: const [
         Text("Status: Available",
             style: TextStyle(
               fontSize: 16,
-            )
+              color: Colors.black,
+              fontWeight: FontWeight.bold,
+            )),
+      ],
+    );
+  }
+
+  Row _buttons() {
+    return Row(
+      mainAxisSize: MainAxisSize.max,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        SizedBox(
+          width: 300,
+          child: OutlinedButton.icon(
+            label: const Text('Lent'),
+            icon: const Icon(Icons.qr_code_rounded),
+            style: OutlinedButton.styleFrom(
+                primary: Colors.white,
+                side: const BorderSide(width: 1.0, color: Colors.white)),
+            onPressed: () {
+              // TODO: Lent
+            },
+          ),
         )
       ],
     );
   }
+
 }
