08968de2ee3fbfb1b09fb782633e22b86749c608
klima7 <ukasz.klimkiewicz2@wp.pl>
2021-11-23 20:58:12 +0100

    Details screen for borrowed items done

--- frontend/lendme/lib/screens/other/item_details/panel_borrowed.dart
+++ frontend/lendme/lib/screens/other/item_details/panel_borrowed.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:lendme/components/user_view.dart';
 import 'package:lendme/models/item.dart';
 import 'package:lendme/models/rental.dart';
 import 'package:lendme/repositories/rental_repository.dart';
@@ -21,13 +23,113 @@ class PanelBorrowed extends StatelessWidget {
   Widget _buildFromRental(BuildContext context, AsyncSnapshot<Rental?> rentalSnap) {
     final rental = rentalSnap.data;
 
+    if(rental == null) {
+      return Container();
+    } else {
+      return _mainLayout(rental);
+    }
+  }
+
+  Column _mainLayout(Rental rental) {
     return Column(
-      children: const [
-        Text("Status: Borrowed",
-            style: TextStyle(
-              fontSize: 16,
-            )
-        )
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        _header(rental),
+        const SizedBox(height: 16),
+        _BorrowTimes(rental),
+        const SizedBox(height: 16),
+        UserView(userId: rental.ownerId, showContactButtons: true),
+        const SizedBox(height: 16),
+        _buttons(),
+      ],
+    );
+  }
+
+  Widget _header(Rental rental) {
+    return Row(
+      children: [
+        RichText(
+          text: TextSpan(
+              style: const TextStyle(
+                fontSize: 16,
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+              ),
+              children: [
+                const TextSpan(text: 'Status: Borrowed from '),
+                TextSpan(text: rental.ownerFullname)
+              ]
+          ),
+        ),
+      ],
+    );
+  }
+
+  Row _buttons() {
+    return Row(
+      mainAxisSize: MainAxisSize.max,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        OutlinedButton.icon(
+          label: const Text('Extend time'),
+          icon: const Icon(Icons.more_time),
+          style: OutlinedButton.styleFrom(
+              primary: Colors.white,
+              side: const BorderSide(width: 1.0, color: Colors.white)
+          ),
+          onPressed: () {
+            // TODO: Extend time
+          },
+        ),
+        const SizedBox(width: 16),
+        OutlinedButton.icon(
+          label: const Text('Transfer loan'),
+          icon: const Icon(Icons.local_shipping),
+          style: OutlinedButton.styleFrom(
+              primary: Colors.white,
+              side: const BorderSide(width: 1.0, color: Colors.white)
+          ),
+          onPressed: () {
+            // TODO: Transfer loan
+          },
+        ),
+      ],
+    );
+  }
+}
+
+class _BorrowTimes extends StatelessWidget {
+  _BorrowTimes(this.rental, {Key? key}) : super(key: key);
+
+  final Rental rental;
+  final DateFormat format = DateFormat('yyyy/MM/dd kk:mm');
+
+  @override
+  Widget build(BuildContext context) {
+    String start = format.format(rental.startDate.toDate());
+    String end = format.format(rental.endDate.toDate());
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Row(
+          children: [
+            const SizedBox(
+              width: 100,
+              child: Text('Borrow from: '),
+            ),
+            Text(start),
+          ],
+        ),
+        Row(
+          children: [
+            const SizedBox(
+              width: 100,
+              child: Text('Borrow to: '),
+            ),
+            Text(end),
+          ],
+        ),
       ],
     );
   }
