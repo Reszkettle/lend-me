bde919d43d9ce549f715b4d6a721a60222974a9d
Rechu <reszkettle@gmail.com>
2021-12-06 22:24:39 +0100

    Added method for fetching current users requests

--- frontend/lendme/lib/repositories/request_repository.dart
+++ frontend/lendme/lib/repositories/request_repository.dart
@@ -1,12 +1,26 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:lendme/exceptions/exceptions.dart';
 import 'package:lendme/models/request.dart';
 import 'package:cloud_functions/cloud_functions.dart';
 
 class RequestRepository {
-
+  final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
   final FirebaseFirestore firestore = FirebaseFirestore.instance;
   final FirebaseFunctions functions = FirebaseFunctions.instance;
+
+  Stream<List<Request?>> getStreamOfCurrentUserRequests() {
+    return firestore
+        .collection('requests')
+        .where('receivers', arrayContains: firebaseAuth.currentUser!.uid)
+        .snapshots()
+        .map((snapshot) {
+      return snapshot.docs.map((queryDocumentSnapshot) {
+        Map<String, dynamic> map = queryDocumentSnapshot.data();
+        return Request.fromMap(map, queryDocumentSnapshot.id);
+      }).toList();
+    });
+  }
 
   Stream<Request?> getRequestStream(String requestId) {
     return firestore
@@ -14,16 +28,16 @@ class RequestRepository {
         .doc(requestId)
         .snapshots()
         .map((snapshot) {
-          print("Snapshot: ${snapshot.data()}");
-          final r = Request.fromMap(snapshot.data(), snapshot.id);
-          return r;
-      });
+      final r = Request.fromMap(snapshot.data(), snapshot.id);
+      return r;
+    });
   }
 
   Future acceptRequest(String requestId, String? message) async {
     try {
-      HttpsCallable acceptFunction = FirebaseFunctions.instance.httpsCallable('requests-accept');
-      final parameters = <String, dynamic> {
+      HttpsCallable acceptFunction =
+          FirebaseFunctions.instance.httpsCallable('requests-accept');
+      final parameters = <String, dynamic>{
         'requestId': requestId,
         'message': message,
       };
@@ -37,8 +51,9 @@ class RequestRepository {
 
   Future rejectRequest(String requestId, String? message) async {
     try {
-      HttpsCallable rejectFunction = FirebaseFunctions.instance.httpsCallable('requests-reject');
-      final parameters = <String, dynamic> {
+      HttpsCallable rejectFunction =
+          FirebaseFunctions.instance.httpsCallable('requests-reject');
+      final parameters = <String, dynamic>{
         'requestId': requestId,
         'message': message,
       };
@@ -49,5 +64,4 @@ class RequestRepository {
       throw UnknownException();
     }
   }
-
 }
