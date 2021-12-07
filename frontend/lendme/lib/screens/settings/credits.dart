f0afb67f92b67344e35e18f5b36a4b521712fac7
klima7 <ukasz.klimkiewicz2@wp.pl>
2021-12-07 14:04:48 +0100

    Credits screen created

--- frontend/lendme/lib/screens/settings/credits.dart
+++ frontend/lendme/lib/screens/settings/credits.dart
@@ -10,16 +10,41 @@ class Credits extends StatelessWidget {
           title: const Text('Credits'),
           elevation: 0.0
       ),
-      body: Stack(
-          children: const [
-            Placeholder(),
-            Center(child:
-              Text(
-                  "Credits",
-                  style: TextStyle(fontSize: 40, color: Colors.grey, backgroundColor: Colors.white)
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Text(
+              "Thanks for providing great, free images!",
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
               ),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 32),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: const [
+                Image(
+                  image: AssetImage("assets/images/logo.png"),
+                  height: 130,
+                ),
+                Expanded(
+                  child: Text(
+                    "Icons made by Freepik from www.flaticon.com",
+                    style: TextStyle(
+                      fontSize: 17,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ],
             ),
           ],
+        ),
       ),
     );
   }
